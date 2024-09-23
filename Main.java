578d1a290af99d04c73dacf4817e4b37d695a8fd
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-09-23 10:13:27 -0700

    justin es muy hermoso

--- Main.java
+++ Main.java
@@ -2,12 +2,28 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    System.out.println(distanceFormula(1,1,2,2));
+    System.out.println(negativeQuadraticFormula(1, 5, 6));
+    System.out.println(positiveQuadraticFormula(1, 5, 6));
   }
 
-  // Implement distance formula method here
+  public static double distanceFormula(double x1, double y1, double x2, double y2)
+  {
+    double distance = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    return distance;
+  }
 
-  // implement one of your quadratic formula methods here
+  public static double negativeQuadraticFormula(double a, double b, double c)
+  {
+    double discriminant = Math.sqrt(b*b-4*a*c);
+    return (-b - discriminant)/(2*a);
+  }
 
-  // implement the other quadratic formula here
+  public static double positiveQuadraticFormula(double a, double b, double c)
+  {
+    double discriminant = Math.sqrt(b*b-4*a*c);
+    return (-b + discriminant)/(2*a);
+  }
 }
+
+
